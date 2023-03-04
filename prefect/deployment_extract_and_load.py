e68e9791ec92f803b6e7963cc0a2d90d42feb1d0
Oleg Agapov <oleg.agapov.ua@gmail.com>
2023-03-04 23:25:10 +0100

    Update Prefect flow

--- prefect/deployment_extract_and_load.py
+++ prefect/deployment_extract_and_load.py
@@ -7,14 +7,14 @@ from prefect_gcp.cloud_run import CloudRunJob
 #client = get_client()
 
 github_block = GitHub.load('de-zoomcamp-project')
-cloud_run_job_block = CloudRunJob.load('de-zoomcamp-project')
+# cloud_run_job_block = CloudRunJob.load('de-zoomcamp-project')
 
 
 deployment = Deployment.build_from_flow(
     flow=extract_and_load,
     name='Extract and Load Github raw data',
     storage=github_block,
-    infrastructure=cloud_run_job_block,
+    #infrastructure=cloud_run_job_block,
 )
 
 if __name__ == '__main__':
