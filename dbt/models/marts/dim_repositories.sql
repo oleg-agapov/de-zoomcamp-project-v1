dba7236c1fe8bc3ac589baeadc75f376529658d9
Oleg Agapov <oleg.agapov.ua@gmail.com>
2023-03-25 17:52:03 +0100

    Fix dim_repositories

--- dbt/models/marts/dim_repositories.sql
+++ dbt/models/marts/dim_repositories.sql
@@ -16,14 +16,12 @@
 {% endif %}
 
 
-select 
+select distinct
     repo_id,
-	repo_name,
-    max(created_at) as load_time
+	  repo_name,
+    max(created_at) over() as load_time
 from {{ ref('stg_github__events') }}
 
 {% if is_incremental() %}
 where created_at >= timestamp('{{ prev_load_time }}')
 {% endif %}
-
-group by 1, 2
